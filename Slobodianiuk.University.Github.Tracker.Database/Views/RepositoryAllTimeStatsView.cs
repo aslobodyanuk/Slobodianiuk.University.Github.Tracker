0f3d31e92204dad9399d6182d32d7d86423904b0
Andrii Slobodianiuk <slobodyanukdev@gmail.com>
2023-06-05 10:14:17 +0200

    Update view to avoid huge numbers in the chart

--- Slobodianiuk.University.Github.Tracker.Database/Views/RepositoryAllTimeStatsView.cs
+++ Slobodianiuk.University.Github.Tracker.Database/Views/RepositoryAllTimeStatsView.cs
@@ -9,14 +9,32 @@ IF OBJECT_ID('{Name}', 'V') IS NOT NULL
     DROP VIEW {Name}
 GO
 
-CREATE VIEW {Name} AS
-SELECT [Date],
-    SUM([Additions]) AS Additions,
-    SUM([Deletions]) AS Deletions,
-    SUM([Total]) AS Total,
-    SUM([FilesCount]) AS FilesCount
-FROM {RepositoryStatsView.Name}
-GROUP BY {RepositoryStatsView.Name}.[Date]
+SELECT
+	[Date],
+	SUM([Additions]) AS Additions,
+	SUM([Deletions]) AS Deletions,
+	SUM([Total]) AS Total,
+	SUM([FilesCount]) AS FilesCount
+FROM
+(
+	SELECT
+	[Date],
+	[FilesCount],
+	CASE
+		WHEN Additions > 1000 THEN 1000
+		ELSE Additions
+	END AS Additions,
+	CASE
+		WHEN Deletions > 1000 THEN 1000
+		ELSE Deletions
+	END AS Deletions,
+	CASE
+		WHEN Total > 1000 THEN 1000
+		ELSE Total
+	END AS Total
+	FROM {RepositoryStatsView.Name}
+) AS Query
+GROUP BY [Query].[Date]
 ";
 
         public const string RemoveView = $"DROP VIEW {Name};";
